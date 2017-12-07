5f837f624ac952ffa74d35e535b486185317c447
Jözhik <uwe.laas@domla.de>
2017-12-07 20:51:39 +0100

    Make configuration work

---
+++ D2.UserManagement/Controllers/UsersController.fs
@@ -0,0 +1,30 @@
+﻿namespace D2.UserManagement.Controllers
+
+open D2.Common
+open D2.UserManagement
+open D2.UserManagement.Persistence
+open Microsoft.AspNetCore.Http
+open Microsoft.AspNetCore.Mvc
+
+[<Route("[controller]")>]
+type UsersController () =
+    inherit Controller()
+
+    let logger = Logger.get "D2.UserManagement.Controllers.UsersController"
+
+    [<HttpPost("register")>]
+    member this.Post() =
+        let user = this.Request.Body.AsUtf8String()
+        match Registration.register user with
+        | Success Ok        -> StatusCodeResult(StatusCodes.Status200OK)
+                               :> ActionResult
+        | Success Known     -> StatusCodeResult(StatusCodes.Status301MovedPermanently)
+                               :> ActionResult
+        | Success Conflict  -> StatusCodeResult(StatusCodes.Status409Conflict)
+                               :> ActionResult
+        | InternalFailure s -> logger.error s "register"
+                               StatusCodeResult(StatusCodes.Status500InternalServerError)
+                               :> ActionResult
+        | ExternalFailure s -> logger.info s
+                               StatusCodeResult(StatusCodes.Status422UnprocessableEntity)
+                               :> ActionResult
---
+++ D2.UserManagement/ServiceRegistration.fs
@@ -0,0 +1,55 @@
+﻿namespace D2.UserManagement
+
+open D2.Common
+open Newtonsoft.Json
+open System
+open System.Net
+open System.Net.Http
+open System.Net.Http.Headers
+open System.Text
+
+module ServiceRegistration =
+    let private logger = Logger.get "D2.UserManagement.ServiceRegistration"
+
+    type EndPoint() =
+        member val Name = String.Empty with get, set
+        member val Uri = String.Empty with get, set
+
+    type Service() =
+        member val Name = "UserManagement" with get
+        member val BaseUrl = sprintf "%s://%s:%d"
+                                     configuration.Hosting.Protocol
+                                     configuration.Hosting.Host
+                                     configuration.Hosting.Port with get
+        member val Version = configuration.Self.Version with get
+        member val Patch = configuration.Self.Patch with get
+        member val EndPoints = [|
+                                   EndPoint(Name = "Frontend", Uri = "/");
+                                   EndPoint(Name = "Register", Uri = "/users/register");
+                               |] with get
+
+    let registerSelf () =
+        use client = new HttpClient()
+        let url = sprintf "%s://%s:%d"
+                          configuration.Broker.Protocol
+                          configuration.Broker.Host
+                          configuration.Broker.Port
+        
+        logger.debug(sprintf "Trying to register with %s" url)
+        
+        client.BaseAddress <- new Uri(url)
+        client.DefaultRequestHeaders.Accept.Clear()
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"))
+        
+        let textData = JsonConvert.SerializeObject(new Service())
+        let content = new StringContent(textData, Encoding.UTF8, "application/json")
+        
+        logger.trace(sprintf "Sending registration %s" textData)
+        
+        try
+            let result = client.PutAsync("/apps/Domla2/01/register", content).Result
+            logger.info(sprintf "Registration yielded %s" result.ReasonPhrase)
+            result.StatusCode = HttpStatusCode.OK
+        with
+        | error -> logger.error error "Failed to contact service broker"
+                   false
