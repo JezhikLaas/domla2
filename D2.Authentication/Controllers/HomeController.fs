268b23b24ae1098715cfbe59ba1f7fe8ffffa954
Jözhik <uwe.laas@domla.de>
2018-01-10 06:00:16 +0100

    Add error handling

--- D2.Authentication/Controllers/HomeController.fs
+++ D2.Authentication/Controllers/HomeController.fs
@@ -1,14 +1,47 @@
 ﻿namespace D2.Authentication
 
+open D2.Common
 open Microsoft.AspNetCore.Mvc
+open Microsoft.AspNetCore.Hosting
+open IdentityServer4.Models
+open IdentityServer4.Services
 
+[<SecurityHeaders>]
 type HomeController
      (
+         interaction : IIdentityServerInteractionService,
+         host : IHostingEnvironment
      ) =
     inherit Controller()
 
     member this.Index () =
         async {
             return VirtualFileResult ("~/index.html", "text/html")
+        }
+        |> Async.StartAsTask
+
+    member this.Error (errorId : string) =
+        async {
+            let errorResult (error : string) (description : string) =
+                match description = null with
+                | true  -> RedirectResult (
+                               sprintf "/app/error?error=%s&description=%s"
+                                       (error.Html())
+                                       (description.Html())
+                           )
+                | false -> RedirectResult (
+                               sprintf "/app/error?error=%s" (error.Html())
+                           )
+            
+            let message = interaction.GetErrorContextAsync(errorId)
+                          |> Async.AwaitTask
+                          |> Async.RunSynchronously
+
+            match message = null with
+            | false -> match host.IsDevelopment () with
+                       | true  -> return errorResult message.Error message.ErrorDescription
+                       | false -> return errorResult message.Error null
+            
+            | true  -> return errorResult "Unbekannter Fehler" null
         }
         |> Async.StartAsTask
---
+++ D2.Common/Html.fs
@@ -0,0 +1,8 @@
+﻿[<AutoOpen>]
+module StringExtensions
+    open System
+    open System.Net
+
+    type String with
+        member this.Html() =
+            WebUtility.HtmlEncode (this)
