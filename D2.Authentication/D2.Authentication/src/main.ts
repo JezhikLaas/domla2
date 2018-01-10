b5dbb6e0015a35375d6b38de3ea78d565ba1ca69
Jözhik <uwe.laas@domla.de>
2018-01-10 19:53:50 +0100

    Provide injectable API_URL

--- D2.Authentication/D2.Authentication/src/main.ts
+++ D2.Authentication/D2.Authentication/src/main.ts
@@ -1,12 +1,16 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { API_URL } from './url';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+
+platformBrowserDynamic([{
+  provide: API_URL,
+  useValue: environment.apiUrl
+}]).bootstrapModule(AppModule)
+   .catch(err => console.log(err));
