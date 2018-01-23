c2ada3cd44c0627b390f2f41b726dd9bc9d22630
Uwe Laas <u.laas@fielmann.com>
2018-01-23 16:49:21 +0100

    Fix service broker tests (WIP)

--- D2.ServiceBroker/StartupTesting.fs
+++ D2.ServiceBroker/StartupTesting.fs
@@ -9,6 +9,24 @@ open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
 open System.IdentityModel.Tokens.Jwt
 
+open Microsoft.AspNetCore.Authorization
+open Microsoft.AspNetCore.Authorization.Infrastructure
+open Microsoft.AspNetCore.Identity
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Authentication.Cookies
+
+
+type DummyAuthorizationHandler () =
+    inherit AuthorizationHandler<OperationAuthorizationRequirement> () with
+
+        override this.HandleRequirementAsync
+                      (
+                        context : AuthorizationHandlerContext,
+                        requirement : OperationAuthorizationRequirement
+                      ) =
+            context.Succeed requirement
+            Task.FromResult(0)
+            :> Task
 
 type StartupTesting private () =
     new (configuration: IConfiguration) as this =
@@ -29,12 +47,25 @@ type StartupTesting private () =
                                     |> ignore
                             )
             )
+            .AddSingleton<IAuthorizationHandler, DummyAuthorizationHandler>()
+            .AddIdentity<IdentityRole, IdentityRole>()
+            .AddDefaultTokenProviders()
             |> ignore
+        
+        services.ConfigureApplicationCookie(fun options ->
+            options.Events <- CookieAuthenticationEvents(
+                                  OnRedirectToLogin = fun context ->
+                                                          context.RedirectUri <- ""
+                                                          Task.FromResult 0 :> Task
+                              )
+        )
+        |> ignore
 
     member this.Configure(app: IApplicationBuilder, env: IHostingEnvironment) =
         app
             .UseCors("default")
             .UseMvc()
+            .UseAuthentication()
             |> ignore
 
     member val Configuration : IConfiguration = null with get, set
