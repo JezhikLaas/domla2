5f2f5bee47aaad45105bc165a19993b23d628f74
Jözhik <uwe.laas@domla.de>
2018-03-05 05:27:37 +0100

    Start rabbit integration (WIP)

---
+++ D2.ServiceBroker/ServiceConnection.fs
@@ -0,0 +1,37 @@
+﻿namespace D2.ServiceBroker
+
+module ServiceConnection =
+    open RabbitMQ.Client
+    open System
+
+    type ServiceConnector (queue : string) as this =
+        
+        let factory = new ConnectionFactory(HostName = "localhost")
+
+        do
+            this.Connection <- factory.CreateConnection ()
+            this.Channel <- this.Connection.CreateModel ()
+            let queueOk = this.Channel.QueueDeclare(
+                              queue = queue,
+                              durable = false,
+                              exclusive = false,
+                              autoDelete = false
+                          )
+            
+            ()
+        
+        member val Connection : IConnection = null with get, set
+        member val Channel : IModel = null with get, set
+
+        member this.Dispose (disposing : bool) =
+            if this.Connection <> null then this.Connection.Dispose ()
+            if this.Channel <> null then this.Channel.Dispose ()
+            ()
+
+        interface IDisposable with
+            member this.Dispose () =
+                this.Dispose true
+        
+        member this.Post (message : string) =
+            ()
+
