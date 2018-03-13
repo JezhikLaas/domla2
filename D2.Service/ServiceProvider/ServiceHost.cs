07dbf9ed3afbe7d705e8cc1e5086b0e3bc67fc91
Jözhik <uwe.laas@domla.de>
2018-03-13 05:16:55 +0100

    Basic service startup (WIP)

---
+++ D2.Service/ServiceProvider/ServiceHost.cs
@@ -0,0 +1,12 @@
+namespace D2.Service.ServiceProvider
+{
+    public class ServiceHost
+    {
+        static public ServiceHostBuilder CreateDefaultBuilder()
+        {
+            var result = new ServiceHostBuilder();
+
+            return result;
+        }
+    }
+}
