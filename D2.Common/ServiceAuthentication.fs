0cc196a175b98aa9699eda802af08da992b3ece4
Jözhik <uwe.laas@domla.de>
2018-04-17 05:14:21 +0200

    #121 Endpoints declared

--- D2.Common/ServiceAuthentication.fs
+++ D2.Common/ServiceAuthentication.fs
@@ -15,6 +15,5 @@ module ServiceAuthentication =
                                    options.ClientId <- "api"
                                    options.ClientSecret <- "78C2A2A1-6167-45E4-A9D7-46C5D921F7D5"
                                    options.SaveToken <- true
-                                   options.EnableCaching <- true
                                    options.CacheDuration <- TimeSpan.FromMinutes 10.0
                 )
