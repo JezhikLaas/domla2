13e7c396b231acddb6d62e872ca46aa0f522ec90
Jözhik <uwe.laas@domla.de>
2018-05-29 05:52:32 +0200

    #121 Make header line sticky

--- D2.Welcome/D2.Welcome/src/app/app.component.ts
+++ D2.Welcome/D2.Welcome/src/app/app.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { LoaderComponent } from './shared/loader/loader.component';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'app';
+export class AppComponent implements OnInit, AfterViewInit {
+  title = 'Domla/2';
   @ViewChild(LoaderComponent) loader: LoaderComponent;
 
   ngOnInit(): void {
     this.loader.show();
-    // this.loader.hide();
+    this.loader.hide();
+  }
+
+  ngAfterViewInit(): void {
+    $(document).ready(function() {
+      $('.ui.sticky')
+        .sticky({
+          context: '#appheader'
+        });
+    });
   }
 }
