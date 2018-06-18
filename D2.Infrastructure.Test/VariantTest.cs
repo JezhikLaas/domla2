7b2eb67244d123b00ef5c4c58f65f1330314e5b2
Jözhik <uwe.laas@domla.de>
2018-06-18 21:29:10 +0200

    #155 Variant Type

---
+++ D2.Infrastructure.Test/VariantTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace D2.Infrastructure.Test
+{
+    public class VariantTest
+    {
+        [Fact(DisplayName = "Variant from decimal types Variant as decimla")]
+        public void TypedValueOperatorPlus()
+        {
+            var target = new Variant(1.0m);
+            Assert.Equal(VariantTag.Decimal, target.Tag);
+        }
+    }
+}
