ef1a7005ba6a625b76256ae78b24a59b313cf46d
Jözhik <uwe.laas@domla.de>
2018-06-19 05:08:14 +0200

    #155 Variant Type

--- D2.Infrastructure.Test/VariantTest.cs
+++ D2.Infrastructure.Test/VariantTest.cs
@@ -5,11 +5,25 @@ namespace D2.Infrastructure.Test
 {
     public class VariantTest
     {
-        [Fact(DisplayName = "Variant from decimal types Variant as decimla")]
-        public void TypedValueOperatorPlus()
+        [Fact(DisplayName = "Variant from decimal types Variant as decimal")]
+        public void VariantFromDecimalYieldsDecimalTyping()
         {
             var target = new Variant(1.0m);
             Assert.Equal(VariantTag.Decimal, target.Tag);
+        }
+        
+        [Fact(DisplayName = "Variant from datetime types Variant as datetime")]
+        public void VariantFromDatetimeYieldsDatetimeTyping()
+        {
+            var target = new Variant(new DateTime(2000, 1, 1));
+            Assert.Equal(VariantTag.DateTime, target.Tag);
+        }
+        
+        [Fact(DisplayName = "Variant from string types Variant as string")]
+        public void VariantFromStringYieldsStringTyping()
+        {
+            var target = new Variant("Hello");
+            Assert.Equal(VariantTag.String, target.Tag);
         }
     }
 }
